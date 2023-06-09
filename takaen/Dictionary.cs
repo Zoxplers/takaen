999b98226113d078bd6a426c56337f2a2c8cee6f
Matt A <Zoxpler@gmail.com>
2023-06-09 09:05:39 -0600

    0.2 Complete
    Added menu strip and translate panel.
    Added more data checks.

---
+++ takaen/Dictionary.cs
@@ -0,0 +1,22 @@
+﻿namespace takaen
+{
+    internal class Dictionary : List<List<List<string>>>
+    {
+        //Constructor
+        internal Dictionary(FileHandler fileHandler)
+        {
+            foreach (int key in Enum.GetValues(typeof(Keys)))
+            {
+                List<List<string>> i = new List<List<string>>();
+                this.Add(i);
+                foreach (int language in Enum.GetValues(typeof(Languages)))
+                {
+                    List<string> j = new List<string>();
+                    i.Add(j);
+                }
+            }
+
+            fileHandler.GetKeyData(this);
+        }
+    }
+}
